5b094a250089b125a35a456fc9c0cb5afa86aef9
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-06-25 18:16:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,30 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+const downloadImage=(url)=>{
+	return new Promise((resolve,reject)=>{
+		fetch(url)
+			.then((res)=>res.json())
+			.then((data)=>resolve(data))
+			.catch((error)=>reject(error))
+	})
+}
+ 
+const download=()=>{
+	const promises=images.map((image)=>downloadImage(image.url))
+	console.log(promises)
+	return Promise.all(promises)
+}
+
+btn.addEventListener('click',()=>{
+	console.log('clicked')
+	download()
+	// download()
+	// 	.then((values)=>{
+	// 		console.log(values)
+	// 	})
+	// 	.catch((error)=>{
+	// 		console.error(error)
+	// 	})
+})
