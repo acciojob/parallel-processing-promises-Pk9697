ef4157892e2d3545e4a62c023f26127bb21ad6c6
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-06-25 18:46:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //your JS code here. If required.
+
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
 
@@ -9,28 +10,28 @@ const images = [
 ];
 
 const downloadImage=(url)=>{
-	return new Promise((resolve,reject)=>{
+	return new Promise((resolve,reject)=>{		
 		fetch(url)
-			.then((res)=>res.json())
-			.then((data)=>resolve(data))
-			.catch((error)=>reject(error))
+			.then((res)=>{
+				resolve(url)
+			})
+			.catch((error)=>reject(`Failed to load image's URL: ${url}`))
 	})
 }
  
 const download=()=>{
 	const promises=images.map((image)=>downloadImage(image.url))
-	console.log(promises)
 	return Promise.all(promises)
 }
 
 btn.addEventListener('click',()=>{
-	console.log('clicked')
-	download()
 	// download()
-	// 	.then((values)=>{
-	// 		console.log(values)
-	// 	})
-	// 	.catch((error)=>{
-	// 		console.error(error)
-	// 	})
+	download()
+		.then((values)=>{
+			output.innerHTML=values.map((url)=>`<img src=${url} />`).join(' ')
+			
+		})
+		.catch((error)=>{
+			console.error(error)
+		})
 })
